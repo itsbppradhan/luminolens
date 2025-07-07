60962cb360a0903e98fcd25246e8349626d957df
itsbppradhan <birajaprasad2468@gmail.com>
2025-07-07 16:58:56 +0530

    refine

--- src/app/video/[id]/page.tsx
+++ src/app/video/[id]/page.tsx
@@ -24,15 +24,15 @@ export default async function VideoPage({ params }: VideoShowcaseProps) {
   return (
     <div className="max-w-6xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-2">{video.title}</h1>
-      <p className="text-gray-600 dark:text-gray-300 mb-4">
-        {video.description || 'No description provided.'}
-      </p>
 
       
       <MediaPlayer playsInline title={video.title} src={video.file || 'https://files.vidstack.io/sprite-fight/720p.mp4'}>
         <MediaProvider />
         <DefaultVideoLayout thumbnails={video.fileThumb || 'https://files.vidstack.io/sprite-fight/thumbnails.vtt'} icons={defaultLayoutIcons} />
       </MediaPlayer>
+      <p className="text-gray-600 dark:text-gray-300 mb-4 mt-6">
+        {video.description || 'No description provided.'}
+      </p>
     </div>
   );
 }
