fc32eee8f8a013e9c424835e2d97db8670ff8dda
itsbppradhan <144833404+itsbppradhan@users.noreply.github.com>
2025-07-07 07:41:18 +0530

    Update page.tsx

--- src/app/video/[id]/page.tsx
+++ src/app/video/[id]/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 import { MediaPlayer, MediaProvider } from '@vidstack/react'
 import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default'
